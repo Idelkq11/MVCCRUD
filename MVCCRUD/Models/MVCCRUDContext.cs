9fcd59eb8c0df3caa9b6ac7fb3d4b84f757aab7a
Idelkq11 <rodriguezidelka17@gmail.com>
2025-01-29 10:35:47 -0400

    Agregar archivos de proyecto.

---
+++ MVCCRUD/Models/MVCCRUDContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MVCCRUD.Models
+{
+    public partial class MVCCRUDContext : DbContext
+    {
+        public MVCCRUDContext()
+        {
+        }
+
+        public MVCCRUDContext(DbContextOptions<MVCCRUDContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuarios { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseSqlServer("Server=localhost;Database=MVCCRUD;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Fecha)
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Clave)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ MVCCRUD/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MVCCRUD.Models
+{
+    public partial class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public DateTime? Fecha { get; set; }
+        public string Clave { get; set; } = string.Empty;
+    }
+}
